b0feb6637c90818cb05cdcbc72c8f4656f59652f
Sudhir Jonathan <sudhir.j@gmail.com>
2012-09-17 23:26:22 +0530

    Just output headers

---
+++ blip/blip.go
@@ -0,0 +1,34 @@
+package hello
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+func init() {
+	http.HandleFunc("/", handler)
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+  w.Header().Add("Content-Type", "application/json")
+
+	m := map[string]string{
+		"latLong": r.Header.Get("X-AppEngine-CityLatLong"),
+		"city":    r.Header.Get("X-AppEngine-City"),
+    "region": r.Header.Get("X-AppEngine-Region"),
+    "country": r.Header.Get("X-AppEngine-Country"),
+    "source": "https://github.com/sudhirj/blip",
+	}
+
+	js, err := json.Marshal(m)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+
+	fmt.Fprint(w, string(js))
+
+}
