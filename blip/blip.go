7910ebfec777d9180f339526a62e8b2edc67bcfe
Sudhir Jonathan <sudhir.j@gmail.com>
2012-11-01 00:00:07 +0530

    moved code files back into subdirectory

--- blip/blip.go
+++ blip/blip.go
@@ -17,7 +17,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		"latLong": r.Header.Get("X-AppEngine-CityLatLong"),
 		"city":    r.Header.Get("X-AppEngine-City"),
 		"region":  r.Header.Get("X-AppEngine-Region"),
-		"country": r.Header.Get("X-AppEngine-Country")
+		"country": r.Header.Get("X-AppEngine-Country"),
 	}
 
 	js, err := json.Marshal(m)
